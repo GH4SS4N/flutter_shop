2b14eeaf0681858d6b07a3a41c0fffdb5dce8af8
Ghassan <x.ghassan.x@gmail.com>
2021-04-15 02:27:37 +0300

    kl

---
+++ lib/CDropdownMenu.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'main.dart';
+//import 'package:namaa_employee/pages/employee/searchpage/search.dart';
+//import 'package:parse_server_sdk/parse_server_sdk.dart';
+
+//import '../../main.dart';
+
+// final storeTypeSelectedProvider = StateProvider<String>((ref) => null);
+final countrySelectedProvider = StateProvider<String>((ref) => "asdf");
+// final citySelectedProvider = StateProvider<String>((ref) => "جدة");
+final programSelectedProvider = StateProvider<String>((ref) => null);
+
+class CDropdown extends ConsumerWidget {
+  final StateProvider selectedState;
+  final List<String> options;
+
+  CDropdown({@required this.selectedState, @required this.options});
+
+  @override
+  Widget build(BuildContext context, watch) {
+    final selected = watch(selectedState).state;
+
+    return Container(
+      height: 68,
+      padding: EdgeInsets.all(24),
+      child: DropdownButton<String>(
+        disabledHint: Text('جاري التحميل'),
+        isExpanded: true,
+        value: options == null ? null : selected ?? options[0],
+        items: options == null
+            ? null
+            : options
+                .map(
+                  (String item) => DropdownMenuItem<String>(
+                    child: Text(item),
+                    value: item,
+                  ),
+                )
+                .toList(),
+        isDense: true,
+        underline: Container(),
+        onChanged: (String newValue) =>
+            context.read(selectedState).state = newValue,
+      ),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(1)),
+          border: Border.all(color: green)),
+    );
+  }
+}
--- lib/userPage.dart
+++ lib/userPage.dart
@@ -3,6 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_shop/item.dart';
 import 'main.dart';
 
+final programsProvider = StateProvider<List<ParseObject>>((ref) => null);
+
 class UserPage extends ConsumerWidget {
   Future<void> _showMyDialog(context) async {
     return showDialog<void>(
@@ -10,23 +12,326 @@ class UserPage extends ConsumerWidget {
       barrierDismissible: false, // user must tap button!
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('AlertDialog Title'),
+          title: Text('انشاء حساب موضف جديد'),
           content: SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                Text('This is a demo alert dialog.'),
-                Text('Would you like to approve of this message?'),
+              child: Container(
+            //color: green,
+            //height: 600,
+            width: 700,
+            child: Column(
+              children: [
+                Icon(
+                  Icons.person_add,
+                  size: 60,
+                  color: green,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "معلومات الحسابات",
+                      style: TextStyle(fontSize: 35, color: green),
+                    ),
+                  ],
+                ),
+                // name - phone number ==================================
+                Row(children: [
+                  Container(
+                    //color: darkgray,
+                    padding: EdgeInsets.all(10),
+                    height: 70,
+                    width: 300,
+                    child: TextFormField(
+                      decoration: new InputDecoration(
+                        suffixIcon: Icon(Icons.phone),
+                        labelText: "رقم الجوال",
+                        fillColor: Colors.white,
+                        border: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(2.0),
+                          borderSide: new BorderSide(),
+                        ),
+                        //fillColor: Colors.green
+                      ),
+                      validator: (val) {
+                        if (val.length == 0) {
+                          return "Email cannot be empty";
+                        } else {
+                          return null;
+                        }
+                      },
+                      // inputFormatters: [
+                      //   WhitelistingTextInputFormatter.digitsOnly
+                      // ],
+                    ),
+                  ),
+                  Spacer(),
+                  Container(
+                    //color: darkgray,
+                    padding: EdgeInsets.all(10),
+                    height: 70,
+                    width: 300,
+                    child: TextFormField(
+                      decoration: new InputDecoration(
+                        suffixIcon: Icon(Icons.phone),
+                        labelText: "رقم الجوال",
+                        fillColor: Colors.white,
+                        border: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(2.0),
+                          borderSide: new BorderSide(),
+                        ),
+                        //fillColor: Colors.green
+                      ),
+                      validator: (val) {
+                        if (val.length == 0) {
+                          return "Email cannot be empty";
+                        } else {
+                          return null;
+                        }
+                      },
+                      // inputFormatters: [
+                      //   WhitelistingTextInputFormatter.digitsOnly
+                      // ],
+                    ),
+                  ),
+                ]),
+                // imployee id num drope dpwn =============================================
+                Row(children: [
+                  Container(
+                    //color: darkgray,
+                    padding: EdgeInsets.all(10),
+                    height: 70,
+                    width: 300,
+                    child: TextFormField(
+                      decoration: new InputDecoration(
+                        suffixIcon: Icon(Icons.phone),
+                        labelText: "رقم الجوال",
+                        fillColor: Colors.white,
+                        border: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(2.0),
+                          borderSide: new BorderSide(),
+                        ),
+                        //fillColor: Colors.green
+                      ),
+                      validator: (val) {
+                        if (val.length == 0) {
+                          return "Email cannot be empty";
+                        } else {
+                          return null;
+                        }
+                      },
+                      // inputFormatters: [
+                      //   WhitelistingTextInputFormatter.digitsOnly
+                      // ],
+                    ),
+                  ),
+                  Spacer(),
+                  Container(
+                    //color: darkgray,
+                    padding: EdgeInsets.all(10),
+                    height: 70,
+                    width: 300,
+                    child: CDropdown(
+                        selectedState: programSelectedProvider,
+                        options: programs == null
+                            ? null
+                            : programs
+                                .map((parsepbject) =>
+                                    parsepbject.get("name").toString())
+                                .toList()),
+                  ),
+                ]),
+                Row(
+                  children: [
+                    Text(
+                      "كلمة المرور",
+                      style: TextStyle(fontSize: 35, color: green),
+                    ),
+                  ],
+                ),
+                // passwords =========================================
+                Row(children: [
+                  Container(
+                    //color: darkgray,
+                    padding: EdgeInsets.all(10),
+                    height: 70,
+                    width: 300,
+                    child: TextFormField(
+                      //obscureText: true,
+                      decoration: new InputDecoration(
+                        suffixIcon: Icon(Icons.lock),
+                        labelText: "كلمة المرور",
+                        fillColor: Colors.white,
+                        border: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(2.0),
+                          borderSide: new BorderSide(),
+                        ),
+                        //fillColor: Colors.green
+                      ),
+                      validator: (val) {
+                        if (val.length == 0) {
+                          return "Email cannot be empty";
+                        } else {
+                          return null;
+                        }
+                      },
+                      // inputFormatters: [
+                      //   WhitelistingTextInputFormatter.digitsOnly
+                      // ],
+                    ),
+                  ),
+                  Spacer(),
+                  Container(
+                    //color: darkgray,
+                    padding: EdgeInsets.all(10),
+                    height: 70,
+                    width: 300,
+                    child: TextFormField(
+                      obscureText: true,
+                      decoration: new InputDecoration(
+                        suffixIcon: Icon(Icons.lock),
+                        labelText: "كلمة المرور",
+                        fillColor: Colors.white,
+                        border: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(2.0),
+                          borderSide: new BorderSide(),
+                        ),
+                        //fillColor: Colors.green
+                      ),
+                      validator: (val) {
+                        if (val.length == 0) {
+                          return "Email cannot be empty";
+                        } else {
+                          return null;
+                        }
+                      },
+                      // inputFormatters: [
+                      //   WhitelistingTextInputFormatter.digitsOnly
+                      // ],
+                    ),
+                  ),
+                ]),
+                Row(
+                  children: [
+                    Text(
+                      "التقييم",
+                      style: TextStyle(fontSize: 35, color: green),
+                    ),
+                  ],
+                ),
+                // http reveiw and results ========================================
+                Row(children: [
+                  Container(
+                    //color: darkgray,
+                    padding: EdgeInsets.all(10),
+                    height: 70,
+                    width: 300,
+                    child: TextFormField(
+                      decoration: new InputDecoration(
+                        suffixIcon: Icon(Icons.phone),
+                        labelText: "رقم الجوال",
+                        fillColor: Colors.white,
+                        border: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(2.0),
+                          borderSide: new BorderSide(),
+                        ),
+                        //fillColor: Colors.green
+                      ),
+                      validator: (val) {
+                        if (val.length == 0) {
+                          return "Email cannot be empty";
+                        } else {
+                          return null;
+                        }
+                      },
+                      // inputFormatters: [
+                      //   WhitelistingTextInputFormatter.digitsOnly
+                      // ],
+                    ),
+                  ),
+                  Spacer(),
+                  Container(
+                    //color: darkgray,
+                    padding: EdgeInsets.all(10),
+                    height: 70,
+                    width: 300,
+                    child: TextFormField(
+                      decoration: new InputDecoration(
+                        suffixIcon: Icon(Icons.phone),
+                        labelText: "رقم الجوال",
+                        fillColor: Colors.white,
+                        border: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(2.0),
+                          borderSide: new BorderSide(),
+                        ),
+                        //fillColor: Colors.green
+                      ),
+                      validator: (val) {
+                        if (val.length == 0) {
+                          return "Email cannot be empty";
+                        } else {
+                          return null;
+                        }
+                      },
+                      // inputFormatters: [
+                      //   WhitelistingTextInputFormatter.digitsOnly
+                      // ],
+                    ),
+                  ),
+                ]),
+                Row(
+                  children: [
+                    Text(
+                      "الفرع",
+                      style: TextStyle(fontSize: 35, color: green),
+                    ),
+                  ],
+                ),
+                // branch number =============================================
+                Row(children: [
+                  Expanded(
+                    //color: darkgray,
+                    // padding: EdgeInsets.all(10),
+                    // height: 70,
+                    // width: 600,
+                    child: Container(
+                      height: 70,
+                      // child: CDropdown(),
+                    ),
+                  ),
+                ]),
+                Container(
+                  height: 50,
+                ),
+                // actions ================================
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: FlatButton(
+                        height: 59,
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Text(
+                          "حفظ",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        color: green,
+                      ),
+                    ),
+                    FlatButton(
+                      height: 59,
+                      onPressed: () {},
+                      child: Text(
+                        "الغاء",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      color: darkgray,
+                    ),
+                  ],
+                )
               ],
             ),
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: Text('Approve'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
+          )),
         );
       },
     );
@@ -34,6 +339,7 @@ class UserPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, watch) {
+    final programs = watch(programsProvider).state;
     var h = MediaQuery.of(context).size.height;
     var w = MediaQuery.of(context).size.width;
     final _pageProvider = watch(pageProvider).state;
@@ -45,7 +351,7 @@ class UserPage extends ConsumerWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
-                margin: EdgeInsets.symmetric(horizontal: 80, vertical: 30),
+                margin: EdgeInsets.symmetric(horizontal: 50, vertical: 30),
                 width: 400,
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -78,14 +384,14 @@ class UserPage extends ConsumerWidget {
                       ),
                     ),
                     FlatButton(
-                      height: 50,
+                      height: 59,
                       onPressed: () {},
                       child: Text(
                         "بحث",
                         style: TextStyle(color: Colors.white),
                       ),
                       color: green,
-                    )
+                    ),
                   ],
                 ),
               ),
@@ -94,11 +400,15 @@ class UserPage extends ConsumerWidget {
                 margin: EdgeInsets.all(20),
                 // padding: EdgeInsets.all(20),
                 child: GridView.count(
-                  crossAxisCount: w < h
-                      ? 3
-                      : h / w <= 13 / 9
-                          ? 2
-                          : 5,
+                  crossAxisCount: w <= 600
+                      ? 2
+                      : w <= 800
+                          ? 3
+                          : w <= 1100
+                              ? 4
+                              : w <= 1500
+                                  ? 5
+                                  : 6,
                   children: [
                     Item(),
                     Item(),
@@ -132,3 +442,5 @@ class UserPage extends ConsumerWidget {
     );
   }
 }
+
+class CDropdown {}
